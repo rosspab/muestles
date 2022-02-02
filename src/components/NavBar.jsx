8f9fec84e1333da3555c546281da26cd0459849c
Pablo Soto <filmcastudios@gmail.com>
2022-02-01 19:22:26 -0600

    NavBar realizado.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return(
+        <header className="header">
+            <nav className="nav">
+                <li className="list-nav">
+                    <a href="#">
+                    <img src="http://placehold.it/" alt="placeholder"/>
+                    </a>
+                </li>
+                <li className="list-nav">
+                    <a href="#">New</a>
+                </li>
+                <li className="list-nav">
+                    <a href="#">White Line</a>
+                </li>
+                <li className="list-nav">
+                    <a href="#">Kitchen</a>
+                </li>
+                <li className="list-nav">
+                    <a href="#">Principal Room</a>
+                        <ul>
+                            <li className="list-nav">Sofa</li>
+                            <li className="list-nav">Desktop</li>
+                            <li className="list-nav">Furniture</li>
+                            <li className="list-nav">Glass Table</li>
+                        </ul>
+                </li>
+                <li className="list-nav">
+                    <a href="#">Ingresar</a>
+                </li>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
